403c35de8c7fe1d29f671a7f9dbc97e63b593200
Marion Jung <marionjung34@hotmail.com>
2023-05-17 08:58:13 +0100

    Updated for more efficiency

--- src/scrabble.js
+++ src/scrabble.js
@@ -1,38 +1,21 @@
 class Scrabble {
   constructor(word) {
-    // Error handling - in case value of null or undefined
-    if (word === null || word === undefined) {
-      this.word = '';
-      // Error handling - converting word to lowercases so the program accepts an input with capital letters
-    } else {
-      this.word = word.toLowerCase();
-    }
+    this.word = (typeof word === 'string' ? word.toLowerCase().trim() : '');
   }
 
-  // Function contains for loop to go through each character & if statements defining the value for each letter
   score() {
     let totalScore = 0;
 
+    const letters = {
+      a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1,
+      s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3,
+      f: 4, h: 4, v: 4, w: 4, y: 4, k: 5,
+      j: 8, x: 8, q: 10, z: 10
+    };
+
     for (let i = 0; i < this.word.length; i++) {
-      const letter = this.word[i];
-      if (["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"].includes(letter)) {
-        totalScore += 1;
-      } else if (["d", "g"].includes(letter)) {
-        totalScore += 2;
-      } else if (["b", "c", "m", "p"].includes(letter)) {
-        totalScore += 3;
-      } else if (["f", "h", "v", "w", "y"].includes(letter)) {
-        totalScore += 4;
-      } else if (["k"].includes(letter)) {
-        totalScore += 5;
-      } else if (["j", "x"].includes(letter)) {
-        totalScore += 8;
-      } else if (["q", "z"].includes(letter)) {
-        totalScore += 10;
-        // Error handling - in case of an empty string
-      } else {
-        totalScore += 0;
-      }
+      let letter = this.word[i];
+      totalScore += letters[letter] || 0;
     }
 
     return totalScore;
